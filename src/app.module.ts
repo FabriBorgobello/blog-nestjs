0e10c2e0aee115a35a3bd929c812f8399c4c4d08
Fabricio Borgobello <fborgobello@gmail.com>
2023-03-26 16:08:38 +0200

    Dockerfile and docker-compose

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,9 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { PostModule } from './post/post.module';
 import { CommentModule } from './comment/comment.module';
+import { HealthModule } from './health/health.module';
 
 @Module({
-  imports: [AuthModule, UserModule, PostModule, CommentModule],
+  imports: [AuthModule, UserModule, PostModule, CommentModule, HealthModule],
 })
 export class AppModule {}
