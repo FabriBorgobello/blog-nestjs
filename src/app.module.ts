68d019ba3ef4fdcf106fcc48874f4270d62e6957
Fabricio Borgobello <fborgobello@gmail.com>
2023-03-26 17:24:00 +0200

    Add prisma module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,16 @@ import { UserModule } from './user/user.module';
 import { PostModule } from './post/post.module';
 import { CommentModule } from './comment/comment.module';
 import { HealthModule } from './health/health.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [AuthModule, UserModule, PostModule, CommentModule, HealthModule],
+  imports: [
+    PrismaModule,
+    AuthModule,
+    UserModule,
+    PostModule,
+    CommentModule,
+    HealthModule,
+  ],
 })
 export class AppModule {}
